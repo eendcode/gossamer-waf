a8ff98c56abae039e82bfb3acd963cd82be37415
rutger <rutger@eenden.eu>
2026-04-15 15:44:21 +0200

    temporary workaround

--- internal/plugins/coraza/coraza.go
+++ internal/plugins/coraza/coraza.go
@@ -331,14 +331,23 @@ func (w *WAF) Validate(c gossamer.Connection) bool {
 	}
 
 	if strikes >= int64(w.MaxStrikes) {
+		// logger.Warn(
+		// 	"blocking request due to high strike level",
+		// 	"cookie", c.Cookie,
+		// 	"ip_address", c.IpAddress,
+		// 	"strikes", strikes,
+		// 	"url", c.Request.RequestURI,
+		// )
+		// return false
+
 		logger.Warn(
-			"blocking request due to high strike level",
+			"should block this due to high strike level, but continuing for now",
 			"cookie", c.Cookie,
 			"ip_address", c.IpAddress,
 			"strikes", strikes,
 			"url", c.Request.RequestURI,
 		)
-		return false
+
 	}
 
 	ok, remaining, err := tokenBucketLimiter.Allow(c.Request.Context(), c.Cookie)
